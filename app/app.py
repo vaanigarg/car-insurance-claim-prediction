a022163c1fa067b82b47bb2c42cdf59c6116e2a3
Vaani Garg <vaanigarg2003@gmail.com>
2026-03-23 20:14:53 +0530

    Final fix

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import joblib
 import pandas as pd
 
 model = joblib.load("models/xgb_model_v2.pkl")
+columns = joblib.load("models/columns.pkl")
 
 st.title("Car Insurance Claim Prediction")
 
@@ -12,7 +13,7 @@ uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
 
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
-    
+    input_df = input_df.reindex(columns-columns, fill_value=0)
     st.write("### Uploaded Data Preview")
     st.dataframe(input_df.head())
 
